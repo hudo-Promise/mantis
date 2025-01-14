32a71b6863d57055fa9945195d365ae253fa1f76
s1ew9lk <extern.dong.hu@audi.com.cn>
2025-01-14 15:07:02 +0800

    fix

--- mantis/models/mantis_config.py
+++ mantis/models/mantis_config.py
@@ -4,6 +4,7 @@ from mantis.models import mantis_db
 class MantisMappingRule(mantis_db.Model):
     __tablename__ = 'mantis_mapping_rule'
     id = mantis_db.Column(mantis_db.Integer, primary_key=True, comment='主键')
+    project = mantis_db.Column(mantis_db.Integer, nullable=True, comment='所属项目')
     cluster_id = mantis_db.Column(mantis_db.Integer, nullable=True, comment='cluster 配置')
     mapping_name = mantis_db.Column(mantis_db.String(32), nullable=True, comment='mapping 名称')
     mapping_rule = mantis_db.Column(mantis_db.JSON, nullable=True, comment='mapping 配置')
